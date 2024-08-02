4d7c153edee84ba4c5c2e1ef9f6bfb4979c1704b
micodecarlos <78213328+micodecarlos@users.noreply.github.com>
2024-08-01 20:33:45 -0500

    Create ejercicio04.cpp

---
+++ Ejercicio04/ejercicio04.cpp
@@ -0,0 +1,18 @@
+//ESTUDIANTE: CARLOS APAZA FLORES
+
+// Sumar dos numeros a + b
+
+#include <iostream>
+
+int main() {
+    int a, b;
+    
+    std::cout << "Ingrese dos numeros enteros: ";
+    std::cin >> a >> b;
+    
+    int suma = a + b;
+    
+    std::cout << "La suma de los dos numeros es: " << suma << std::endl;
+    
+    return 0;
+}
